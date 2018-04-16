465c3a0b2298e0ee952fe9e12f9a95f184fb53df
VarghaSabee <vargha.sabolch@gmail.com>
2018-04-16 18:10:25 +0200

    ClearDB

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -43,7 +43,8 @@ class AdminLoginController extends Controller
             return redirect()->intended(route('admin.dashboard'));
         }
         // if unsuccessful, then redirect back to the login with the form data
-        return redirect()->back()->withInput($request->only('email', 'remember'));
+       // return redirect()->back()->withInput($request->only('email', 'remember'));
+        dd($request);
     }
 
     public function logout()
